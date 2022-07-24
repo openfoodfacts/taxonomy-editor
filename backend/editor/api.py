4def1a64da1dc265b555bb11744f82b4f938f6eb
Aadarsh A <aadarsh.ram@gmail.com>
2022-07-25 02:31:32 +0530

    Added GET and PUT for entries

--- backend/editor/api.py
+++ backend/editor/api.py
@@ -1,9 +1,9 @@
 from datetime import datetime
+from neo4j import GraphDatabase
 
 # FastAPI
-from fastapi import FastAPI, status, Response, Depends, Header, HTTPException, Cookie
+from fastapi import FastAPI, status, Response, Request
 from fastapi.responses import JSONResponse
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Open Food Facts Taxonomy Editor API")
@@ -16,7 +16,7 @@ app.add_middleware(
     # But that will only allow certain types of communication, excluding everything that involves 
     # credentials: Cookies, Authorization headers like those used with Bearer Tokens, etc.
     # So, for everything to work correctly, it's better to specify explicitly the allowed origins."
-    # => Workarround: use allow_origin_regex
+    # => Workaround: use allow_origin_regex
     # Source: https://github.com/tiangolo/fastapi/issues/133#issuecomment-646985050
     allow_origin_regex='https?://.*',
     allow_credentials=True,
@@ -24,6 +24,24 @@ app.add_middleware(
     allow_headers=["*"],
     expose_headers=["*"],
 )
+
+@app.on_event("startup")
+async def startup():
+    """
+    Initialize Neo4J database
+    """
+    global driver, session
+    uri = "bolt://localhost:7687"
+    driver = GraphDatabase.driver(uri)
+    session = driver.session()
+
+@app.on_event("shutdown")
+async def shutdown():
+    """
+    Close session and driver of Neo4J database
+    """
+    session.close()
+    driver.close()
 
 @app.get("/", status_code=status.HTTP_200_OK)
 async def hello():
@@ -36,3 +54,97 @@ async def pong(response: Response):
     """
     pong = datetime.now()
     return {"ping": "pong @ %s" % pong}
+
+@app.get("/entry/{entry}")
+async def findOneEntry(response: Response, entry: str):
+    """
+    Get entry corresponding to id within taxonomy
+    """
+    query = """
+        MATCH (n:ENTRY) WHERE n.id = $id 
+        RETURN n
+    """
+    result = session.run(query, {"id": entry})
+    oneEntry = [record for record in result]
+    return oneEntry
+
+@app.get("/entriesfull")
+async def findAllEntries(response: Response):
+    """
+    Get all entries within taxonomy
+    """
+    query = """
+        MATCH (n:ENTRY) RETURN n
+    """
+    result = session.run(query)
+    allEntries = [record for record in result]
+    return allEntries
+
+@app.get("/synonym/{synonym}")
+async def findOneEntry(response: Response, synonym: str):
+    """
+    Get synonym corresponding to id within taxonomy
+    """
+    query = """
+        MATCH (n:SYNONYMS) WHERE n.id = $id 
+        RETURN n
+    """
+    result = session.run(query, {"id": synonym})
+    oneSynonym = [record for record in result]
+    return oneSynonym
+
+@app.get("/synonymsfull")
+async def findAllSynonyms(response: Response):
+    """
+    Get all synonyms within taxonomy
+    """
+    query = """
+        MATCH (n:SYNONYMS) RETURN n
+    """
+    result = session.run(query)
+    allSyononyms = [record for record in result]
+    return allSyononyms
+
+@app.get("/stopword/{stopword}")
+async def findOneEntry(response: Response, stopword: str):
+    """
+    Get stopword corresponding to id within taxonomy
+    """
+    query = """
+        MATCH (n:STOPWORDS) WHERE n.id = $id 
+        RETURN n
+    """
+    result = session.run(query, {"id": stopword})
+    oneStopword = [record for record in result]
+    return oneStopword
+
+@app.get("/stopwordsfull")
+async def findAllStopwords(response: Response):
+    """
+    Get all stopwords within taxonomy
+    """
+    query = """
+        MATCH (n:STOPWORDS) RETURN n
+    """
+    result = session.run(query)
+    allStopwords = [record for record in result]
+    return allStopwords
+
+@app.put("/edit/entry/{entry}")
+async def editEntry(request: Request, entry: str):
+    """
+    Editing an entry in a taxonomy.
+    New key-value pairs can be added, old key-value pairs can be updated.
+    URL will be of format '/edit/<id>'
+    """
+    incomingData = await request.json()
+    result = None
+    for key in incomingData.keys():
+        query = f"""
+            MATCH (n:ENTRY) WHERE n.id = $id
+            SET n.{key} = $value
+            RETURN n
+        """
+        result = session.run(query, {"id": entry, "value": incomingData[key]})
+    updatedEntry = [record for record in result]
+    return updatedEntry
