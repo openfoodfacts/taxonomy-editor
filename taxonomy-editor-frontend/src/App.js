3eb91493f9e70180d7c212360e61ebfca5ad8755
Aadarsh A <aadarsh.ram@gmail.com>
2022-08-16 23:34:02 +0530

    feat: All entries page + Navbar (#43)
    * Add home screen
    
    * Added navbar and an all entries page
    
    * Requested changes (part 1)
    
    * Change useFetch
    
    * Change vars
    
    * Add requested changes
    
    * Remove isSuccess check

--- taxonomy-editor-frontend/src/App.js
+++ taxonomy-editor-frontend/src/App.js
@@ -1,5 +1,7 @@
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
+import Entry from "./pages/allentries";
 import Home from './pages/home';
 
 const theme = createTheme({
@@ -13,9 +15,11 @@ function App() {
     <ThemeProvider theme={theme}>
     <CssBaseline />
     <Router>
+        <ResponsiveAppBar />
         <div className="App">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/entry" element={<Entry />} />
           </Routes>
         </div>
     </Router>
