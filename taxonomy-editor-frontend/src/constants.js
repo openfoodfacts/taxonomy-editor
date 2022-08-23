e6bd990f5e0bfc89f86e4e7c8f259cec127cea10
Alex Garel <alex@garel.org>
2022-08-23 20:07:55 +0200

    build: setting up docker-compose for dev

--- taxonomy-editor-frontend/src/constants.js
+++ taxonomy-editor-frontend/src/constants.js
@@ -1,2 +1,2 @@
-// FIXME get from ENV
-export const API_URL = "http://localhost:80/"
+// FIXME we should find a way that works for production build
+export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:80/"
