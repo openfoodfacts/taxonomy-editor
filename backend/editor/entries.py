44025da3b204ee7209554b1447d4669507ec3b13
Aadarsh A <aadarsh.ram@gmail.com>
2022-09-06 21:43:27 +0530

    feat: Add GET paths for parents and children + fix: Update Neo4J query (#35)

--- backend/editor/entries.py
+++ backend/editor/entries.py
@@ -3,7 +3,7 @@ Database helper functions for API
 """
 import re
 from neo4j import GraphDatabase         # Interface with Neo4J
-from . import settings                         # Neo4J settings
+from . import settings                  # Neo4J settings
 
 def initialize_db():
     """
@@ -43,6 +43,28 @@ def get_nodes(label, entry):
     result = session.run(query, {"id": entry})
     return result
 
+def get_parents(entry):
+    """
+    Helper function used for getting node parents with given id
+    """
+    query = f"""
+        MATCH (a:ENTRY)-[r:is_child_of]->(b) WHERE a.id = $id 
+        RETURN b.id
+    """
+    result = session.run(query, {"id": entry})
+    return result
+
+def get_children(entry):
+    """
+    Helper function used for getting node children with given id
+    """
+    query = f"""
+        MATCH (b)-[r:is_child_of]->(a:ENTRY) WHERE a.id = $id 
+        RETURN b.id
+    """
+    result = session.run(query, {"id": entry})
+    return result
+
 def update_nodes(label, entry, incomingData):
     """
     Helper function used for updation of node with given id and label
@@ -52,9 +74,25 @@ def update_nodes(label, entry, incomingData):
         if not re.match(r"^\w+$", key) or key == "id":
             raise ValueError("Invalid key: %s", key)
     
-    # Build query
-    query = [f"""MATCH (n:{label}) WHERE n.id = $id"""]
+    # Get current node information and deleted keys
+    curr_node = list(get_nodes(label, entry).data()[0]['n'].keys())
+    deleted_keys = (set(curr_node) ^ set(incomingData))
 
+    # Check for keys having null/empty values
+    for key in curr_node:
+        if (curr_node[key] == []) or (curr_node[key] == None):
+            deleted_keys.add(key)
+
+    # Build query
+    query = [f"""MATCH (n:{label}) WHERE n.id = $id """]
+
+    # Delete keys removed by user
+    for key in deleted_keys:
+        if key == "id": # Doesn't require to be deleted
+            continue
+        query.append(f"""\nREMOVE n.{key}\n""")
+
+    # Update keys
     for key in incomingData.keys():
         query.append(f"""\nSET n.{key} = ${key}\n""")
 
