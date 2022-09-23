531b1e824c6a32bc342f2c7b97bf3cdad54f59c4
Aadarsh A <aadarsh.ram@gmail.com>
2022-09-24 01:41:50 +0530

    Change line max limit for flake8

--- parser/openfoodfacts_taxonomy_parser/parser.py
+++ parser/openfoodfacts_taxonomy_parser/parser.py
@@ -1,4 +1,4 @@
-from logging import basicConfig, FileHandler, INFO
+import logging
 import re
 import unicodedata
 import sys
@@ -422,7 +422,7 @@ class Parser:
     def delete_used_properties(self):
         query = "MATCH (n) SET n.is_before = null, n.parents = null"
         self.session.run(query)
-
+    
     def create_fulltext_index(self):
         query = "CREATE FULLTEXT INDEX nodeSearch FOR (n:ENTRY) ON EACH [n.id]"
         self.session.run(query)
@@ -437,7 +437,7 @@ class Parser:
 
 
 if __name__ == "__main__":
-    basicConfig(handlers=[FileHandler(filename="parser.log", encoding="utf-8")], level=INFO)
+    logging.basicConfig(handlers=[logging.FileHandler(filename="parser.log", encoding="utf-8")], level=logging.INFO)
     filename = sys.argv[1] if len(sys.argv) > 1 else "test"
     parse = Parser()
     parse(filename)
