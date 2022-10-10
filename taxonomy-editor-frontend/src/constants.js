9fbf43e2a63557ddd2d29dede84301213cdfd39f
Alex Garel <alex@garel.org>
2022-10-10 16:56:47 +0200

    build: improve api host discovery
    fixes:
    - #62

--- taxonomy-editor-frontend/src/constants.js
+++ taxonomy-editor-frontend/src/constants.js
@@ -1,2 +1,18 @@
-// FIXME we should find a way that works for production build
-export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:80/"
+/**
+ * Build the taxonomy api url from ui url
+ * @param URL location
+ * @returns string
+ */
+function taxonomyApiUrlFromUi(location) {
+    let components = location.host.split(".");
+    if (components[0] === "ui") {
+        // we build api url by just replacing ui by api
+        components[0] = "api";
+        return location.protocol + "//" + components.join(".") + "/"
+    }
+    else {
+        // this is a default for simple dev setup
+        return "http://localhost:80/"
+    }
+}
+export const API_URL = taxonomyApiUrlFromUi(window.location);
