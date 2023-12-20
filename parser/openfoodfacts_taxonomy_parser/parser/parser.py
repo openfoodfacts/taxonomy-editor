1098bc7bcd7cee018022b0ee94d382cc214628db
Eric Nguyen <ericnguyen.developper@gmail.com>
2023-12-20 11:49:10 +0100

    fix: remove multi_label for single project_label

--- parser/openfoodfacts_taxonomy_parser/parser/parser.py
+++ parser/openfoodfacts_taxonomy_parser/parser/parser.py
@@ -28,17 +28,21 @@ class Parser:
         self.session = session
         self.parser_logger = ParserConsoleLogger()
 
-    def _create_headernode(self, header: list[str], multi_label: str):
+    def _get_project_name(self, taxonomy_name: str, branch_name: str):
+        """Create a project name for given branch and taxonomy"""
+        return "p_" + taxonomy_name + "_" + branch_name
+
+    def _create_headernode(self, header: list[str], project_label: str):
         """Create the node for the header"""
         query = f"""
-                CREATE (n:{multi_label}:TEXT)
+                CREATE (n:{project_label}:TEXT)
                 SET n.id = '__header__'
                 SET n.preceding_lines= $header
                 SET n.src_position= 1
             """
         self.session.run(query, header=header)
 
-    def _create_node(self, node_data: NodeData, multi_label: str):
+    def _create_node(self, node_data: NodeData, project_label: str):
         """Run the query to create the node with data dictionary"""
         position_query = """
             SET n.id = $id
@@ -48,13 +52,13 @@ class Parser:
         """
         entry_query = ""
         if node_data.get_node_type() == NodeType.TEXT:
-            id_query = f" CREATE (n:{multi_label}:TEXT) \n "
+            id_query = f" CREATE (n:{project_label}:TEXT) \n "
         elif node_data.get_node_type() == NodeType.SYNONYMS:
-            id_query = f" CREATE (n:{multi_label}:SYNONYMS) \n "
+            id_query = f" CREATE (n:{project_label}:SYNONYMS) \n "
         elif node_data.get_node_type() == NodeType.STOPWORDS:
-            id_query = f" CREATE (n:{multi_label}:STOPWORDS) \n "
+            id_query = f" CREATE (n:{project_label}:STOPWORDS) \n "
         else:
-            id_query = f" CREATE (n:{multi_label}:ENTRY) \n "
+            id_query = f" CREATE (n:{project_label}:ENTRY) \n "
             position_query += " SET n.main_language = $main_language "
             if node_data.parent_tag:
                 entry_query += " SET n.parents = $parent_tag \n"
@@ -69,33 +73,24 @@ class Parser:
         query = id_query + entry_query + position_query
         self.session.run(query, node_data.to_dict())
 
-    def _get_project_name(self, taxonomy_name: str, branch_name: str):
-        """Create a project name for given branch and taxonomy"""
-        return "p_" + taxonomy_name + "_" + branch_name
-
-    def _create_multi_label(self, taxonomy_name: str, branch_name: str) -> str:
-        """Create a combined label with taxonomy name and branch name"""
-        project_name = self._get_project_name(taxonomy_name, branch_name)
-        return project_name + ":" + ("t_" + taxonomy_name) + ":" + ("b_" + branch_name)
-
-    def create_nodes(self, nodes: list[NodeData], multi_label: str):
+    def create_nodes(self, nodes: list[NodeData], project_label: str):
         """Adding nodes to database"""
         self.parser_logger.info("Creating nodes")
         for node in nodes:
             if node.id == "__header__":
-                self._create_headernode(node.preceding_lines, multi_label)
+                self._create_headernode(node.preceding_lines, project_label)
             else:
-                self._create_node(node, multi_label)
+                self._create_node(node, project_label)
 
-    def create_previous_link(self, previous_links: list[PreviousLink], multi_label: str):
+    def create_previous_link(self, previous_links: list[PreviousLink], project_label: str):
         self.parser_logger.info("Creating 'is_before' links")
         for previous_link in previous_links:
             id = previous_link["id"]
             before_id = previous_link["before_id"]
 
             query = f"""
-                MATCH(n:{multi_label}) WHERE n.id = $id
-                MATCH(p:{multi_label}) WHERE p.id= $before_id
+                MATCH(n:{project_label}) WHERE n.id = $id
+                MATCH(p:{project_label}) WHERE p.id= $before_id
                 CREATE (p)-[r:is_before]->(n)
                 RETURN r
             """
@@ -111,16 +106,16 @@ class Parser:
             elif not relation[0]:
                 self.parser_logger.error("link not created between %s and %s", id, before_id)
 
-    def create_child_link(self, child_links: list[ChildLink], multi_label: str):
+    def create_child_link(self, child_links: list[ChildLink], project_label: str):
         """Create the relations between nodes"""
         self.parser_logger.info("Creating 'is_child_of' links")
         for child_link in child_links:
             child_id = child_link["id"]
             parent = child_link["parent_id"]
             lc, parent_id = parent.split(":")
-            query = f""" MATCH (p:{multi_label}:ENTRY) WHERE $parent_id IN p.tags_ids_""" + lc
+            query = f""" MATCH (p:{project_label}:ENTRY) WHERE $parent_id IN p.tags_ids_""" + lc
             query += f"""
-                MATCH (c:{multi_label}) WHERE c.id= $child_id
+                MATCH (c:{project_label}) WHERE c.id= $child_id
                 CREATE (c)-[r:is_child_of]->(p)
                 RETURN r
             """
@@ -130,12 +125,11 @@ class Parser:
                     f"parent not found for child {child_id} with parent {parent_id}"
                 )
 
-    def _create_fulltext_index(self, taxonomy_name: str, branch_name: str):
+    def _create_fulltext_index(self, project_label: str):
         """Create indexes for search"""
-        project_name = self._get_project_name(taxonomy_name, branch_name)
         query = (
-            f"""CREATE FULLTEXT INDEX {project_name+'_SearchIds'} IF NOT EXISTS
-            FOR (n:{project_name}) ON EACH [n.id]\n"""
+            f"""CREATE FULLTEXT INDEX {project_label+'_SearchIds'} IF NOT EXISTS
+            FOR (n:{project_label}) ON EACH [n.id]\n"""
             + """
             OPTIONS {indexConfig: {`fulltext.analyzer`: 'keyword'}}"""
         )
@@ -144,15 +138,14 @@ class Parser:
         language_codes = [lang.alpha2 for lang in list(iso639.languages) if lang.alpha2 != ""]
         tags_prefixed_lc = ["n.tags_" + lc for lc in language_codes]
         tags_prefixed_lc = ", ".join(tags_prefixed_lc)
-        query = f"""CREATE FULLTEXT INDEX {project_name+'_SearchTags'} IF NOT EXISTS
-            FOR (n:{project_name}) ON EACH [{tags_prefixed_lc}]"""
+        query = f"""CREATE FULLTEXT INDEX {project_label+'_SearchTags'} IF NOT EXISTS
+            FOR (n:{project_label}) ON EACH [{tags_prefixed_lc}]"""
         self.session.run(query)
 
-    def _create_parsing_errors_node(self, taxonomy_name: str, branch_name: str):
+    def _create_parsing_errors_node(self, taxonomy_name: str, branch_name: str, project_label: str):
         """Create node to list parsing errors"""
-        multi_label = self._create_multi_label(taxonomy_name, branch_name)
         query = f"""
-            CREATE (n:{multi_label}:ERRORS)
+            CREATE (n:{project_label}:ERRORS)
             SET n.id = $project_name
             SET n.branch_name = $branch_name
             SET n.taxonomy_name = $taxonomy_name
@@ -161,7 +154,7 @@ class Parser:
             SET n.errors = $errors_list
         """
         params = {
-            "project_name": self._get_project_name(taxonomy_name, branch_name),
+            "project_name": project_label,
             "branch_name": branch_name,
             "taxonomy_name": taxonomy_name,
             "warnings_list": self.parser_logger.parsing_warnings,
@@ -172,14 +165,14 @@ class Parser:
     def __call__(self, filename: str, branch_name: str, taxonomy_name: str):
         """Process the file"""
         branch_name = normalizing(branch_name, char="_")
-        multi_label = self._create_multi_label(taxonomy_name, branch_name)
+        project_label = self._get_project_name(taxonomy_name, branch_name)
         taxonomy_parser = TaxonomyParser()
         taxonomy = taxonomy_parser.parse_file(filename, self.parser_logger)
-        self.create_nodes([*taxonomy.entry_nodes, *taxonomy.other_nodes], multi_label)
-        self.create_child_link(taxonomy.child_links, multi_label)
-        self.create_previous_link(taxonomy.previous_links, multi_label)
-        self._create_fulltext_index(taxonomy_name, branch_name)
-        self._create_parsing_errors_node(taxonomy_name, branch_name)
+        self.create_nodes([*taxonomy.entry_nodes, *taxonomy.other_nodes], project_label)
+        self.create_child_link(taxonomy.child_links, project_label)
+        self.create_previous_link(taxonomy.previous_links, project_label)
+        self._create_fulltext_index(project_label)
+        self._create_parsing_errors_node(taxonomy_name, branch_name, project_label)
 
 
 if __name__ == "__main__":
