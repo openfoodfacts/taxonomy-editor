3410ad0d1e4f1e0c5c45510e67d9078510bde04a
Eric Nguyen <ericnguyen.developper@gmail.com>
2023-12-20 09:27:06 +0100

    feat: update TaxonomyParser to return taxonomy class

--- parser/openfoodfacts_taxonomy_parser/parser/taxonomy_parser.py
+++ parser/openfoodfacts_taxonomy_parser/parser/taxonomy_parser.py
@@ -1,8 +1,10 @@
 import logging
 import re
 import sys
+import timeit
+from enum import Enum
+from dataclasses import dataclass, field
 from typing import Iterator, TypedDict
-
 
 from .logger import ParserConsoleLogger
 from .exception import DuplicateIDError
@@ -14,13 +16,63 @@ def ellipsis(text, max=20):
     return text[:max] + ("..." if len(text) > max else "")
 
 
-class NodeData(TypedDict):
+class NodeType(str, Enum):
+    TEXT = "TEXT"
+    SYNONYMS = "SYNONYMS"
+    STOPWORDS = "STOPWORDS"
+    ENTRY = "ENTRY"
+
+
+@dataclass(slots=True)
+class NodeData:
+    id: str = ""
+    is_before: str | None = None
+    main_language: str | None = None
+    preceding_lines: list[str] = field(default_factory=list)
+    parent_tag: list[str] = field(default_factory=list)
+    src_position: int | None = None
+    properties: dict[str, str] = field(default_factory=dict)
+    tags: dict[str, list[str]] = field(default_factory=dict)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "is_before": self.is_before,
+            "main_language": self.main_language,
+            "preceding_lines": self.preceding_lines,
+            "parent_tag": self.parent_tag,
+            "src_position": self.src_position,
+            **self.properties,
+            **self.tags,
+        }
+
+    def get_node_type(self):
+        if self.id in ["__header__", "__footer__"]:
+            return NodeType.TEXT
+        elif self.id.startswith("synonyms"):
+            return NodeType.SYNONYMS
+        elif self.id.startswith("stopwords"):
+            return NodeType.STOPWORDS
+        else:
+            return NodeType.ENTRY
+
+
+class PreviousLink(TypedDict):
+    before_id: str
     id: str
-    main_language: str
-    preceding_lines: list[str]
-    parent_tag: list[str]
-    src_position: int | None
-    is_before: str
+
+
+class ChildLink(TypedDict):
+    parent_id: str
+    id: str
+
+
+@dataclass(slots=True)
+class Taxonomy:
+    entry_nodes: list[NodeData]
+    other_nodes: list[NodeData]
+    previous_links: list[PreviousLink]
+    child_links: list[ChildLink]
 
 
 class TaxonomyParser:
@@ -87,21 +139,9 @@ class TaxonomyParser:
             new_line.append(self._remove_stopwords(lc, normalizing(word, lc)))
         return lc, new_line
 
-    def _new_node_data(self, is_before: str) -> NodeData:
-        """To create an empty dictionary that will be used to create node"""
-        data = NodeData(
-            id="",
-            main_language="",
-            preceding_lines=[],
-            parent_tag=[],
-            src_position=None,
-            is_before=is_before,
-        )
-        return data
-
     def _set_data_id(self, data: NodeData, id: str, line_number: int) -> NodeData:
-        if not data["id"]:
-            data["id"] = id
+        if not data.id:
+            data.id = id
         else:
             raise DuplicateIDError(line_number)
         return data
@@ -136,7 +176,7 @@ class TaxonomyParser:
         if line.startswith("stopwords") or line.startswith("synonyms") or not line:
             # can be the end of an block or just additional line separator,
             # file_iter() always end with ''
-            if data["id"]:  # to be sure that it's an end
+            if data.id:  # to be sure that it's an end
                 return True
         return False
 
@@ -145,26 +185,26 @@ class TaxonomyParser:
         To remove the one separating line that is always there,
         between synonyms part and stopwords part and before each entry
         """
-        is_before = data["is_before"]
+        is_before = data.is_before
         # first, check if there is at least one preceding line
-        if data["preceding_lines"] and not data["preceding_lines"][0]:
-            if data["id"].startswith("synonyms"):
+        if data.preceding_lines and not data.preceding_lines[0]:
+            if data.id.startswith("synonyms"):
                 # it's a synonyms block,
                 # if the previous block is a stopwords block,
                 # there is at least one separating line
-                if "stopwords" in is_before:
-                    data["preceding_lines"].pop(0)
+                if is_before and "stopwords" in is_before:
+                    data.preceding_lines.pop(0)
 
-            elif data["id"].startswith("stopwords"):
+            elif data.id.startswith("stopwords"):
                 # it's a stopwords block,
                 # if the previous block is a synonyms block,
                 # there is at least one separating line
-                if "synonyms" in is_before:
-                    data["preceding_lines"].pop(0)
+                if is_before and "synonyms" in is_before:
+                    data.preceding_lines.pop(0)
 
             else:
                 # it's an entry block, there is always a separating line
-                data["preceding_lines"].pop(0)
+                data.preceding_lines.pop(0)
         return data
 
     def _harvest_entries(self, filename: str, entries_start_line: int) -> Iterator[NodeData]:
@@ -180,33 +220,34 @@ class TaxonomyParser:
         # stopwords will contain a list of stopwords with their language code as key
         self.stopwords = {}
         # the other entries
-        data = self._new_node_data(is_before="__header__")
-        data["is_before"] = "__header__"
-        line_number = entries_start_line
+        data = NodeData(is_before="__header__")
+        line_number = (
+            entries_start_line  # if the iterator is empty, line_number will not be unbound
+        )
         for line_number, line in self._file_iter(filename, entries_start_line):
             # yield data if block ended
             if self._entry_end(line, data):
-                if data["id"] in saved_nodes:
+                if data.id in saved_nodes:
                     msg = (
                         "Entry with same id %s already created, "
                         "duplicate id in file at line %s. "
                         "Node creation cancelled."
                     )
-                    self.parser_logger.error(msg, data["id"], data["src_position"])
+                    self.parser_logger.error(msg, data.id, data.src_position)
                 else:
                     data = self._remove_separating_line(data)
                     yield data  # another function will use this dictionary to create a node
-                    saved_nodes.append(data["id"])
-                data = self._new_node_data(is_before=data["id"])
+                    saved_nodes.append(data.id)
+                data = NodeData(is_before=data.id)
 
             # harvest the line
             if not (line) or line[0] == "#":
                 # comment or blank
-                data["preceding_lines"].append(line)
+                data.preceding_lines.append(line)
             else:
                 line = line.rstrip(",")
-                if not data["src_position"]:
-                    data["src_position"] = line_number + 1
+                if not data.src_position:
+                    data.src_position = line_number + 1
                 if line.startswith("stopwords"):
                     # general stopwords definition for a language
                     id = "stopwords:" + str(index_stopwords)
@@ -221,7 +262,7 @@ class TaxonomyParser:
                             ellipsis(line),
                         )
                     else:
-                        data["tags_" + lc] = value
+                        data.tags["tags_" + lc] = value
                         # add the list with its lc
                         self.stopwords[lc] = value
                 elif line.startswith("synonyms"):
@@ -240,17 +281,17 @@ class TaxonomyParser:
                             ellipsis(line),
                         )
                     else:
-                        data["tags_" + lc] = tags
-                        data["tags_ids_" + lc] = value
+                        data.tags["tags_" + lc] = tags
+                        data.tags["tags_ids_" + lc] = value
                 elif line[0] == "<":
                     # parent definition
-                    data["parent_tag"].append(self._add_line(line[1:]))
+                    data.parent_tag.append(self._add_line(line[1:]))
                 elif language_code_prefix.match(line):
                     # synonyms definition
-                    if not data["id"]:
-                        data["id"] = self._add_line(line.split(",", 1)[0])
+                    if not data.id:
+                        data.id = self._add_line(line.split(",", 1)[0])
                         # first 2-3 characters before ":" are the language code
-                        data["main_language"] = data["id"].split(":", 1)[0]
+                        data.main_language = data.id.split(":", 1)[0]
                     # add tags and tagsid
                     lang, line = line.split(":", 1)
                     # to transform '-' from language code to '_'
@@ -263,8 +304,8 @@ class TaxonomyParser:
                         if word_normalized not in tagsids_list:
                             # in case 2 normalized synonyms are the same
                             tagsids_list.append(word_normalized)
-                    data["tags_" + lang] = tags_list
-                    data["tags_ids_" + lang] = tagsids_list
+                    data.tags["tags_" + lang] = tags_list
+                    data.tags["tags_ids_" + lang] = tagsids_list
                 else:
                     # property definition
                     property_name = None
@@ -289,25 +330,57 @@ class TaxonomyParser:
                                 ellipsis(line),
                             )
                         if property_name:
-                            data["prop_" + property_name + "_" + lc] = property_value
+                            data.properties["prop_" + property_name + "_" + lc] = property_value
 
-        data["id"] = "__footer__"
-        data["preceding_lines"].pop(0)
-        data["src_position"] = line_number + 1 - len(data["preceding_lines"])
+        data.id = "__footer__"
+        data.preceding_lines.pop(0)
+        data.src_position = line_number + 1 - len(data.preceding_lines)
         yield data
 
-    def create_nodes(self, filename: str, multi_label: str):
-        """Adding nodes to database"""
-        self.parser_logger.info("Creating nodes")
+    def _create_taxonomy(self, filename: str) -> Taxonomy:
+        """Create the taxonomy from the file"""
+        self.parser_logger.info("Parsing taxonomy file %s", filename)
         harvested_header_data, entries_start_line = self._header_harvest(filename)
+        entry_nodes: list[NodeData] = []
+        other_nodes = [
+            NodeData(id="__header__", preceding_lines=harvested_header_data, src_position=1)
+        ]
+        previous_links: list[PreviousLink] = []
+        child_links: list[ChildLink] = []
         harvested_data = self._harvest_entries(filename, entries_start_line)
         for entry in harvested_data:
-            print(entry)
+            if entry.get_node_type() == NodeType.ENTRY:
+                entry_nodes.append(entry)
+            else:
+                other_nodes.append(entry)
+            if entry.is_before:
+                previous_links.append(PreviousLink(before_id=entry.is_before, id=entry.id))
+            if entry.parent_tag:
+                for parent in entry.parent_tag:
+                    child_links.append(ChildLink(parent_id=parent, id=entry.id))
+        return Taxonomy(
+            entry_nodes=entry_nodes,
+            other_nodes=other_nodes,
+            previous_links=previous_links,
+            child_links=child_links,
+        )
 
-    def __call__(self, filename: str):
-        """Process the file"""
+    def parse_file(self, filename: str, logger: ParserConsoleLogger | None = None) -> Taxonomy:
+        if logger:
+            self.parser_logger = logger
+        """Process the file into a Taxonomy object"""
+        start_time = timeit.default_timer()
         filename = self._normalized_filename(filename)
-        self.create_nodes(filename, "")
+        taxonomy = self._create_taxonomy(filename)
+        end_time = timeit.default_timer()
+        self.parser_logger.info("Parsing done in %s seconds", end_time - start_time)
+        self.parser_logger.info(
+            "Found %d nodes", len(taxonomy.entry_nodes) + len(taxonomy.other_nodes)
+        )
+        self.parser_logger.info("Found %d previous links", len(taxonomy.previous_links))
+        self.parser_logger.info("Found %d child links", len(taxonomy.child_links))
+
+        return taxonomy
 
 
 if __name__ == "__main__":
@@ -317,4 +390,4 @@ if __name__ == "__main__":
 
     # Pass session variable to parser object
     parse = TaxonomyParser()
-    parse(filename)
+    parse.parse_file(filename)
