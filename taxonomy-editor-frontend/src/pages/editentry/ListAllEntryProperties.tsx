7bfcb7153520171e773a2251c8b17c7d331844b2
Piv94165 <106757110+Piv94165@users.noreply.github.com>
2024-02-14 09:28:09 +0100

    fix(frontend): enhance property edition (#394)
    * fix(frontend): replace underscore by colons in property table
    
    * refactor(frontend): convert jsx to tsx
    
    * refactor(frontend): get rid of useless uuid
    
    * feat(frontend): validate property name format
    
    * feat(frontend): check if property name is unique when editing table
    
    * fix(frontend): fix property name validation
    
    ---------
    
    Co-authored-by: alice.juan <alice.juan@student-cs.fr>

--- taxonomy-editor-frontend/src/pages/editentry/ListAllEntryProperties.tsx
+++ taxonomy-editor-frontend/src/pages/editentry/ListAllEntryProperties.tsx
@@ -1,38 +1,45 @@
-import { Box, Grid, Paper, Typography } from "@mui/material";
+import { Alert, Box, Grid, Paper, Snackbar, Typography } from "@mui/material";
 import MaterialTable, { MTableToolbar } from "@material-table/core";
 import { useState } from "react";
+import ISO6391, { LanguageCode } from "iso-639-1";
 
-interface RenderedProperties {
-  id: string;
+type RowType = {
   propertyName: string;
   propertyValue: string;
-}
+};
+
+type RenderedPropertyType = RowType & {
+  id: string;
+};
 
 const ListAllEntryProperties = ({ nodeObject, setNodeObject }) => {
-  const collectProperties = () => {
-    const renderedProperties: RenderedProperties[] = [];
-    Object.keys(nodeObject).forEach((key) => {
-      // Collecting uuids of properties
-      // UUID of properties will have a "_uuid" suffix
-      // Ex: prop_vegan_en_uuid
+  function replaceAll(str: string, find: string, replace: string) {
+    return str.replace(new RegExp(find, "g"), replace);
+  }
 
-      if (
-        key.startsWith("prop") &&
-        key.endsWith("uuid") &&
-        !key.endsWith("_comments_uuid")
-      ) {
-        const uuid = nodeObject[key][0]; // UUID
+  const normalizeNameToFrontend = (name: string) => {
+    // Use the replace() method with a regular expression to replace underscores with colons
+    return replaceAll(name, "_", ":");
+  };
+
+  const normalizeNameToDb = (name: string) => {
+    // Use the replace() method with a regular expression to replace underscores with colons
+    return replaceAll(name, ":", "_");
+  };
+
+  const collectProperties = (): RenderedPropertyType[] => {
+    const renderedProperties: RenderedPropertyType[] = [];
+    Object.keys(nodeObject).forEach((key: string) => {
+      // Collecting properties (begin with prop_)
+      // Ex: prop_vegan_en
+      if (key.startsWith("prop") && !key.endsWith("_comments")) {
         // Removing "prop_" prefix from key to render only the name
-        const property_name = key.split("_").slice(1, -1).join("_");
-
-        // Properties have a prefix "prop_" followed by their name
-        // Getting key for accessing property value
-        const property_key = "prop_" + property_name;
+        const property_name = key.replace(/^prop_/, "");
 
         renderedProperties.push({
-          id: uuid,
-          propertyName: property_name,
-          propertyValue: nodeObject[property_key],
+          id: Math.random().toString(),
+          propertyName: normalizeNameToFrontend(property_name),
+          propertyValue: nodeObject[key],
         });
       }
     });
@@ -42,7 +49,7 @@ const ListAllEntryProperties = ({ nodeObject, setNodeObject }) => {
   const [data, setData] = useState(collectProperties());
 
   // Helper function used for changing properties of node
-  const changePropertyData = (key, value) => {
+  const changePropertyData = (key: string, value: string) => {
     setNodeObject((prevState) => {
       const newNodeObject = { ...prevState };
       newNodeObject["prop_" + key] = value;
@@ -51,12 +58,49 @@ const ListAllEntryProperties = ({ nodeObject, setNodeObject }) => {
   };
 
   // Helper function used for deleting properties of node
-  const deletePropertyData = (key) => {
+  const deletePropertyData = (key: string) => {
     setNodeObject((prevState) => {
-      const keyToRemove = "prop_" + key;
-      const { [keyToRemove]: _propToRemove, ...newNodeObject } = prevState;
+      const toRemove = normalizeNameToDb("prop_" + key);
+      const { [toRemove]: _, ...newNodeObject } = prevState;
       return newNodeObject;
     });
+  };
+
+  const LanguageCodes = ISO6391.getAllCodes();
+
+  const validatePropertyName = (propertyName: string): boolean => {
+    // Every property name should be in the form property_name:lang_code
+    if (propertyName) {
+      const words = propertyName.split(":");
+      const langCode = words[words.length - 1];
+      if (!LanguageCodes.includes(langCode as LanguageCode)) {
+        return false;
+      }
+      // Property name should not include special caracters
+      for (const word of words) {
+        const pattern = /^[a-zA-Z0-9_]+$/;
+        if (!pattern.test(word)) {
+          return false;
+        }
+      }
+      return true;
+    }
+    return false;
+  };
+
+  const isPropertyNameUnique = (
+    propertyName: string,
+    otherProperties: RenderedPropertyType[]
+  ): boolean => {
+    for (const prop of otherProperties) {
+      if (prop.propertyName === propertyName) return false;
+    }
+    return true;
+  };
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const handleCloseErrorSnackbar = () => {
+    setErrorMessage("");
   };
 
   return (
@@ -66,59 +110,96 @@ const ListAllEntryProperties = ({ nodeObject, setNodeObject }) => {
         <MaterialTable
           data={data}
           columns={[
-            { title: "Name", field: "propertyName" },
+            {
+              title: "Name",
+              field: "propertyName",
+              validate: (rowData) =>
+                validatePropertyName(rowData.propertyName)
+                  ? true
+                  : {
+                      isValid: false,
+                      helperText:
+                        "Property name should not contain special caracters and should follow the format : property_name:lang_code",
+                    },
+            },
             { title: "Value", field: "propertyValue" },
           ]}
           editable={{
-            onRowAdd: async (newRow) => {
-              // Add new property to rendered rows
-              const updatedRows = [
-                ...data,
-                { ...newRow, id: Math.random().toString() },
-              ];
-              setData(updatedRows);
+            onRowAdd: (newRow: RowType) =>
+              new Promise<void>((resolve, reject) => {
+                if (!isPropertyNameUnique(newRow.propertyName, data)) {
+                  setErrorMessage(`${newRow.propertyName} already exists`);
+                  reject();
+                } else {
+                  // Add new property to rendered rows
+                  const updatedRows = [
+                    ...data,
+                    { id: Math.random().toString(), ...newRow },
+                  ];
+                  setData(updatedRows);
 
-              // Add new key-value pair of a property in nodeObject
-              changePropertyData(newRow.propertyName, newRow.propertyValue);
-            },
-            onRowDelete: async (selectedRow) => {
-              // Delete property from rendered rows
-              const updatedRows = [...data];
-              const index = parseInt(selectedRow.id);
-              updatedRows.splice(index, 1);
-              setData(updatedRows);
-
-              // Delete key-value pair of a property from nodeObject
-              deletePropertyData(selectedRow.propertyName);
-            },
-            onRowUpdate: async (updatedRow, oldRow) => {
-              // Update row in rendered rows
-              const updatedRows = data.map((el) =>
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-expect-error
-                el.id === oldRow.id ? updatedRow : el
-              );
-              setData(updatedRows);
-              // Updation takes place by deletion + addition
-              // If property name has been changed, previous key should be removed from nodeObject
-              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-              // @ts-expect-error
-              updatedRow.propertyName !== oldRow.propertyName &&
-                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                // @ts-expect-error
-                deletePropertyData(oldRow.propertyName);
-              // Add new property to nodeObject
-              changePropertyData(
-                updatedRow.propertyName,
-                updatedRow.propertyValue
-              );
-            },
+                  // Add new key-value pair of a property in nodeObject
+                  changePropertyData(
+                    normalizeNameToDb(newRow.propertyName),
+                    newRow.propertyValue
+                  );
+                  resolve();
+                }
+              }),
+            onRowDelete: (selectedRow: RenderedPropertyType) =>
+              new Promise<void>((resolve) => {
+                // Delete property from rendered rows
+                const updatedRows = [...data];
+                const index = updatedRows.findIndex(
+                  (row) => row.id === selectedRow.id
+                );
+                updatedRows.splice(index, 1);
+                setData(updatedRows);
+                // Delete key-value pair of a property from nodeObject
+                deletePropertyData(selectedRow.propertyName);
+                resolve();
+              }),
+            onRowUpdate: (
+              updatedRow: RenderedPropertyType,
+              oldRow: RenderedPropertyType
+            ) =>
+              new Promise<void>((resolve, reject) => {
+                const index = data.findIndex((row) => row.id === updatedRow.id);
+                const otherProperties = [...data];
+                otherProperties.splice(index, 1);
+                if (
+                  !isPropertyNameUnique(
+                    updatedRow.propertyName,
+                    otherProperties
+                  )
+                ) {
+                  setErrorMessage(`${updatedRow.propertyName} already exists`);
+                  reject();
+                } else {
+                  // Update row in rendered rows
+                  const updatedRows = data.map((el) =>
+                    el.id === oldRow.id ? updatedRow : el
+                  );
+                  setData(updatedRows);
+                  // Updation takes place by deletion + addition
+                  // If property name has been changed, previous key should be removed from nodeObject
+                  updatedRow.propertyName !== oldRow.propertyName &&
+                    deletePropertyData(oldRow.propertyName);
+                  // Add new property to nodeObject
+                  changePropertyData(
+                    normalizeNameToDb(updatedRow.propertyName),
+                    updatedRow.propertyValue
+                  );
+                  resolve();
+                }
+              }),
           }}
           options={{
             actionsColumnIndex: -1,
             addRowPosition: "last",
             tableLayout: "fixed",
             paging: false,
+            showTitle: false,
           }}
           components={{
             Toolbar: (props) => {
@@ -142,6 +223,21 @@ const ListAllEntryProperties = ({ nodeObject, setNodeObject }) => {
           }}
         />
       </Box>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        open={!!errorMessage}
+        autoHideDuration={3000}
+        onClose={handleCloseErrorSnackbar}
+      >
+        <Alert
+          elevation={6}
+          variant="filled"
+          onClose={handleCloseErrorSnackbar}
+          severity="error"
+        >
+          {errorMessage}
+        </Alert>
+      </Snackbar>
     </Box>
   );
 };
