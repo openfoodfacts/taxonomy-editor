4425dfb53dde90516bf91a58200b1b80aa6db295
alice.juan <alice.juan@student-cs.fr>
2024-03-21 12:24:26 +0100

    refactor(frontend): a file for each main component

--- taxonomy-editor-frontend/src/pages/project/search/FilterInput.tsx
+++ taxonomy-editor-frontend/src/pages/project/search/FilterInput.tsx
@@ -1,38 +1,46 @@
-import { FormControl, InputLabel, OutlinedInput } from "@mui/material"
+import { FormControl, InputLabel, OutlinedInput } from "@mui/material";
 import { Dispatch, SetStateAction } from "react";
 
 type FilterInputType = {
-    label: string,
-    filterValue : string,
-    setFilterValue: Dispatch<SetStateAction<string>>,
-    setQ: Dispatch<SetStateAction<string>>,
-    keySearchTerm: string,
-    setCurrentPage: Dispatch<SetStateAction<number>>,
-}
+  label: string;
+  filterValue: string;
+  setFilterValue: Dispatch<SetStateAction<string>>;
+  setQ: Dispatch<SetStateAction<string>>;
+  keySearchTerm: string;
+  setCurrentPage: Dispatch<SetStateAction<number>>;
+};
 
-export const FilterInput = ({label, filterValue, setFilterValue, setQ, keySearchTerm, setCurrentPage}:FilterInputType) => {
-
-    const addFilter = (event: React.KeyboardEvent<HTMLInputElement>) => {
-        if (event.key === 'Enter' && filterValue!=="") {
-            // If the filterValue includes a space, wrap it in double quotes. Ex : “en: paprika extract”, en:e101ii
-            const value = filterValue.includes(" ")? `"${filterValue}"`: filterValue
-            setCurrentPage(1);
-            setQ((prevQ) => `${prevQ} ${keySearchTerm}:${value}`);
-            event.preventDefault();
-            setFilterValue("");
-        }
+export const FilterInput = ({
+  label,
+  filterValue,
+  setFilterValue,
+  setQ,
+  keySearchTerm,
+  setCurrentPage,
+}: FilterInputType) => {
+  const addFilter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter" && filterValue !== "") {
+      // If the filterValue includes a space, wrap it in double quotes. Ex : “en: paprika extract”, en:e101ii
+      const value = filterValue.includes(" ")
+        ? `"${filterValue}"`
+        : filterValue;
+      setCurrentPage(1);
+      setQ((prevQ) => `${prevQ} ${keySearchTerm}:${value}`);
+      event.preventDefault();
+      setFilterValue("");
     }
+  };
 
-    return (
-        <FormControl sx={{ m: 1, width: "170px" }} variant="outlined">
-            <InputLabel id="multiple-select-label">{label}</InputLabel>
-            <OutlinedInput
-                id="property-input"
-                sx={{backgroundColor:"#f2e9e4"}}
-                value={filterValue}
-                onChange={(event) => setFilterValue(event.target.value)}
-                onKeyDown={addFilter}
-            />
-        </FormControl>
-)
-}
+  return (
+    <FormControl sx={{ m: 1, width: "170px" }} variant="outlined">
+      <InputLabel id="multiple-select-label">{label}</InputLabel>
+      <OutlinedInput
+        id="property-input"
+        sx={{ backgroundColor: "#f2e9e4" }}
+        value={filterValue}
+        onChange={(event) => setFilterValue(event.target.value)}
+        onKeyDown={addFilter}
+      />
+    </FormControl>
+  );
+};
---
+++ taxonomy-editor-frontend/src/pages/project/search/FiltersArea.tsx
@@ -0,0 +1,192 @@
+import { Box, FormControlLabel, Checkbox } from "@mui/material";
+import { FilterInput } from "./FilterInput";
+import { MultipleSelectFilter } from "./MultipleSelectFilter";
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
+import ISO6391 from "iso-639-1";
+import { EntryNodeSearchResult } from "@/client";
+
+const checkboxTheme = {
+  color: "#201a17",
+  "&.Mui-checked": {
+    color: "#341100",
+  },
+};
+
+type FiltersAreaType = {
+  setCurrentPage: Dispatch<SetStateAction<number>>;
+  setQ: Dispatch<SetStateAction<string>>;
+  filters: EntryNodeSearchResult["filters"];
+};
+
+export const FiltersArea = ({
+  setCurrentPage,
+  setQ,
+  filters,
+}: FiltersAreaType) => {
+  const [isRootNodesChecked, setIsRootNodesChecked] = useState<boolean>(true);
+  const [chosenLanguagesCodes, setChosenLanguagesCodes] = useState<string[]>(
+    []
+  );
+  const [withoutChosenLanguagesCodes, setWithoutChosenLanguagesCodes] =
+    useState<string[]>([]);
+  const [parentId, setParentId] = useState<string>("");
+  const [childId, setChildId] = useState<string>("");
+  const [ancestorId, setAncestorId] = useState<string>("");
+  const [descendantId, setDescendantId] = useState<string>("");
+
+  const initializeFilters = (): {
+    isRootNodesChecked: boolean;
+    isModifiedChecked: boolean;
+    chosenLanguagesCodes: string[];
+    withoutChosenLanguagesCodes: string[];
+  } => {
+    return {
+      isRootNodesChecked: false,
+      isModifiedChecked: false,
+      chosenLanguagesCodes: [],
+      withoutChosenLanguagesCodes: [],
+    };
+  };
+
+  const updateFiltersStates = useCallback((updatedFilters) => {
+    const filtersStates = initializeFilters();
+    for (const filter of updatedFilters) {
+      switch (filter.filterType) {
+        case "is":
+          if (filter.filterValue === "root") {
+            filtersStates.isRootNodesChecked = true;
+          } else if (filter.filterValue === "modified") {
+            filtersStates.isModifiedChecked = true;
+          }
+          break;
+        case "language":
+          if (filter.negated) {
+            filtersStates.withoutChosenLanguagesCodes.push(
+              filter.filterValue.replace("not:", "")
+            );
+          } else {
+            filtersStates.chosenLanguagesCodes.push(filter.filterValue);
+          }
+          break;
+        default:
+          break;
+      }
+    }
+    setIsRootNodesChecked(filtersStates.isRootNodesChecked);
+    setChosenLanguagesCodes(filtersStates.chosenLanguagesCodes);
+    setWithoutChosenLanguagesCodes(filtersStates.withoutChosenLanguagesCodes);
+  }, []);
+
+  useEffect(() => {
+    updateFiltersStates(filters);
+  }, [filters, updateFiltersStates]);
+
+  const handleCheckbox = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    filterKey: string,
+    isChecked: boolean,
+    setChecked: React.Dispatch<React.SetStateAction<boolean>>
+  ) => {
+    setCurrentPage(1);
+    if (!isChecked) {
+      setQ((prevQ) => prevQ + ` ${filterKey}`);
+    } else {
+      setQ((prevQ) => prevQ.replace(`${filterKey}`, ""));
+    }
+    setChecked(event.target.checked);
+  };
+
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        m: 1,
+        borderRadius: 1,
+        bgcolor: "#f2e9e4",
+        p: 1,
+        pl: 2,
+        pr: 2,
+        display: "flex",
+        flexWrap: "wrap",
+        alignItems: "center",
+      }}
+    >
+      <FormControlLabel
+        id="root-nodes-checkbox"
+        control={
+          <Checkbox
+            sx={checkboxTheme}
+            onChange={(e) =>
+              handleCheckbox(
+                e,
+                "is:root",
+                isRootNodesChecked,
+                setIsRootNodesChecked
+              )
+            }
+            checked={isRootNodesChecked}
+          />
+        }
+        label="Root nodes"
+      />
+      <MultipleSelectFilter
+        label="Translated into"
+        filterValue={chosenLanguagesCodes}
+        listOfChoices={ISO6391.getAllNames()}
+        mapCodeToValue={ISO6391.getName}
+        mapValueToCode={ISO6391.getCode}
+        setQ={setQ}
+        keySearchTerm="language"
+        setCurrentPage={setCurrentPage}
+      />
+      <MultipleSelectFilter
+        label="Not translated into"
+        filterValue={withoutChosenLanguagesCodes}
+        listOfChoices={ISO6391.getAllNames()}
+        mapCodeToValue={ISO6391.getName}
+        mapValueToCode={ISO6391.getCode}
+        setQ={setQ}
+        keySearchTerm="language:not"
+        setCurrentPage={setCurrentPage}
+      />
+      <FilterInput
+        label="Is parent of (id)"
+        filterValue={parentId}
+        setFilterValue={setParentId}
+        setQ={setQ}
+        keySearchTerm="parent"
+        setCurrentPage={setCurrentPage}
+      />
+      <FilterInput
+        label="Is child of (id)"
+        filterValue={childId}
+        setFilterValue={setChildId}
+        setQ={setQ}
+        keySearchTerm="child"
+        setCurrentPage={setCurrentPage}
+      />
+      <FilterInput
+        label="Is ancestor of (id)"
+        filterValue={ancestorId}
+        setFilterValue={setAncestorId}
+        setQ={setQ}
+        keySearchTerm="ancestor"
+        setCurrentPage={setCurrentPage}
+      />
+      <FilterInput
+        label="Is descendant of (id)"
+        filterValue={descendantId}
+        setFilterValue={setDescendantId}
+        setQ={setQ}
+        keySearchTerm="descendant"
+        setCurrentPage={setCurrentPage}
+      />
+    </Box>
+  );
+};
