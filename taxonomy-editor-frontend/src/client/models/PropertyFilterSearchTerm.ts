7b4bf9d203a86a82850cdea3e9531e20c8148c07
Eric Nguyen <ericnguyen.developper@gmail.com>
2024-03-21 16:37:54 +0100

    chore: generate SDK

---
+++ taxonomy-editor-frontend/src/client/models/PropertyFilterSearchTerm.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type PropertyFilterSearchTerm = {
+  filterType: "property";
+  filterValue: string;
+  readonly negated: boolean;
+  readonly inherited: boolean;
+  readonly propertyName: string;
+  readonly propertyValue: string | null;
+};
