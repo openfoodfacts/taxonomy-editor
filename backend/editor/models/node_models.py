012de098da1f79e90c77ad6943c47378f6abcc86
Alex Garel <alex@garel.org>
2024-05-13 11:21:32 +0200

    fix: node update and display (#497)
    After it was broken by node_models introduction
    
    Removed some API methods that would not work

--- backend/editor/models/node_models.py
+++ backend/editor/models/node_models.py
@@ -1,6 +1,8 @@
+import re
 from enum import StrEnum
 from typing import Any
 
+from openfoodfacts_taxonomy_parser import utils as parser_utils
 from pydantic import model_validator
 
 from .base_models import BaseModel
@@ -29,13 +31,51 @@ class EntryNodeCreate(BaseModel):
 
 class EntryNode(BaseModel):
     id: str
-    preceding_lines: list[str]
+    preceding_lines: list[str] = []
     main_language: str
     tags: dict[str, list[str]]
+    tags_ids: dict[str, list[str]]
     properties: dict[str, str]
     comments: dict[str, list[str]]
     is_external: bool = False
     original_taxonomy: str | None = None
+
+    def flat_dict(self) -> dict[str, Any]:
+        """
+        Flatten the EntryNode model into a single dictionary.
+        """
+        flat_data = dict(self)
+        flat_data.update(self.tags)
+        del flat_data["tags"]
+        flat_data.update(self.tags_ids)
+        del flat_data["tags_ids"]
+        flat_data.update(self.properties)
+        del flat_data["properties"]
+        flat_data.update(self.comments)
+        del flat_data["comments"]
+        return flat_data
+
+    def recompute_tags_ids(self, stopwords: dict[str, list[str]]):
+        """Recompute the tags_ids dictionary based on the tags dictionary
+        and the provided stopwords."""
+        self.tags_ids = {}
+        for key, values in self.tags.items():
+            # Normalise tags and store them in tags_ids
+            keys_language_code = key.split("_", 1)[1]
+            normalised_value = []
+            for value in values:
+                normalised_value.append(
+                    parser_utils.normalize_text(value, keys_language_code, stopwords=stopwords)
+                )
+            self.tags_ids["tags_ids" + key[4:]] = normalised_value
+
+    def recompute_id(self):
+        """Recompute id based on main_language and tag_ids
+
+        You must ensure you called recompute_tags_ids() before calling this method if needed
+        """
+        main_tag_id = self.tags_ids[f"tags_ids_{self.main_language}"][0]
+        self.id = f"{self.main_language}:{main_tag_id}"
 
     @model_validator(mode="before")
     @classmethod
@@ -46,6 +86,11 @@ class EntryNode(BaseModel):
         """
         if not isinstance(data, dict):
             return data
+
+        # Sanity check keys
+        for key in data.keys():
+            if not re.match(r"^\w+$", key):
+                raise ValueError("Invalid key: %s", key)
 
         is_tag_property_or_comment = (
             lambda key: key.startswith("tags_")
@@ -58,16 +103,25 @@ class EntryNode(BaseModel):
         }
 
         parsed_data["tags"] = parsed_data.get("tags", {})
+        parsed_data["tags_ids"] = parsed_data.get("tags_ids", {})
         parsed_data["properties"] = parsed_data.get("properties", {})
         parsed_data["comments"] = parsed_data.get("comments", {})
 
         for key, value in data.items():
+            if value is None or value == []:
+                continue
             if key.endswith("_comments"):
                 parsed_data["comments"][key] = value
             elif key.startswith("tags_"):
-                parsed_data["tags"][key] = value
+                if "_ids_" in key:
+                    parsed_data["tags_ids"][key] = value
+                else:
+                    parsed_data["tags"][key] = value
             elif key.startswith("prop_"):
                 parsed_data["properties"][key] = value
+
+        if parsed_data.get("main_language") is None and parsed_data.get("id"):
+            parsed_data["main_language"] = parsed_data["id"].split(":", 1)[1]
 
         return parsed_data
 
