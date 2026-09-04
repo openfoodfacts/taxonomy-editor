60ecdcd534cf737f98ab93dda77f749a04c143a0
John Gomersall <thegoms@gmail.com>
2026-09-04 18:34:16 +0100

    Better login validation on upload
    Signed-off-by: John Gomersall <thegoms@gmail.com>

--- parser/rdf_export/rdf_upload.py
+++ parser/rdf_export/rdf_upload.py
@@ -28,6 +28,7 @@ def get_authenticated_session():
     response = new_session.post(url, data=payload)
     if response.status_code == 200 and "result" in response.text:
         return new_session
+    print(f"Login failed: {response.text}")
     return None
 
 
@@ -60,93 +61,97 @@ if __name__ == "__main__":
     BASE_URI = str(addTaxonomyNamespace(taxonomy_name))
 
     session = get_authenticated_session()
-    res = session.post(
-        f"{API_BASE}/st-metadata-registry-services/MetadataRegistry/createDatasetAbstraction",
-        data={
-            "shortName": taxonomy_name,
-            "uriSpace": BASE_URI,
-            "title": f'"{taxonomy_name}"@en',
-        },
-    )
-    is_response_ok("Create Dataset:", res)
-
-    res = session.post(
-        f"{CORE_SERVICES_BASE}/Projects/createProject",
-        data={
-            "consumer": "SYSTEM",
-            "projectName": taxonomy_name,
-            "baseURI": BASE_URI,
-            "model": "<http://www.w3.org/2004/02/skos/core>",
-            "lexicalizationModel": "<http://www.w3.org/2004/02/skos/core>",
-            "repositoryAccess": '{"@type":"CreateLocal"}',
-            "supportRepoID": "countries_support",
-            "coreRepoSailConfigurerSpecification": json.dumps(
-                {
-                    "factoryId": f"{CONFIG_NS}.RDF4JRepositoryConfigurer",
-                    "configType": f"{CONFIG_NS}.RDF4JPersistentInMemorySailConfiguration",
-                    "configuration": {
-                        "@type": f"{CONFIG_NS}.RDF4JPersistentInMemorySailConfiguration",
-                        "syncDelay": 1000,
-                        "directTypeInference": False,
-                        "inferencer": "none",
-                    },
-                }
-            ),
-            "historyEnabled": "false",
-            "validationEnabled": "false",
-            "mdrRegistration": json.dumps(
-                {
-                    "datasetAbstraction": f"{ST_URL}/mdr/dataset-{taxonomy_name}-abs",
-                    "version": "1.0.0",
-                    "currentVersion": True,
-                }
-            ),
-            "openAtStartup": "true",
-            # facets
-            # {"@type":"it.uniroma2.art.semanticturkey.settings.facets.ProjectFacets"}
-            # createMainShard
-            # false
-            # appCtx
-            # SHOWVOC
-            # blacklistingEnabled
-            # false
-        },
-    )
-    is_response_ok("Create Project", res)
-
-    res = session.post(f"{CORE_SERVICES_BASE}/Projects/makePublic?ctx_project={taxonomy_name}")
-    if is_response_ok("Make Public", res):
+    if session:
         res = session.post(
-            f"{CORE_SERVICES_BASE}/InputOutput/clearData?ctx_project={taxonomy_name}"
+            f"{API_BASE}/st-metadata-registry-services/MetadataRegistry/createDatasetAbstraction",
+            data={
+                "shortName": taxonomy_name,
+                "uriSpace": BASE_URI,
+                "title": f'"{taxonomy_name}"@en',
+            },
         )
-        if is_response_ok("Clear Data", res):
-            with open(f"{taxonomy_name}.ttl", "rb") as f:
-                res = session.post(
-                    f"{CORE_SERVICES_BASE}/InputOutput/loadRDF?ctx_project={taxonomy_name}",
-                    # /st-core-services/InputOutput/loadRDF?ctx_project=countries&ctx_forceEditable=true&ctx_shard=main
-                    data={
-                        "baseURI": BASE_URI,
-                        "format": "Turtle",
-                        "consumer": "SYSTEM",
-                        "transitiveImportAllowance": "web",
-                        # validateImplicitly
-                        # false
-                    },
-                    files={"inputFile": f},
-                )
-                # Load service just returns a result object which is empty on success
-                res_json = res.json()
-                if res_json.get("result", res_json):
-                    print(f"Load RDF: result: {res_json}")
+        is_response_ok("Create Dataset:", res)
 
+        res = session.post(
+            f"{CORE_SERVICES_BASE}/Projects/createProject",
+            data={
+                "consumer": "SYSTEM",
+                "projectName": taxonomy_name,
+                "baseURI": BASE_URI,
+                "model": "<http://www.w3.org/2004/02/skos/core>",
+                "lexicalizationModel": "<http://www.w3.org/2004/02/skos/core>",
+                "repositoryAccess": '{"@type":"CreateLocal"}',
+                "supportRepoID": "countries_support",
+                "coreRepoSailConfigurerSpecification": json.dumps(
+                    {
+                        "factoryId": f"{CONFIG_NS}.RDF4JRepositoryConfigurer",
+                        "configType": f"{CONFIG_NS}.RDF4JPersistentInMemorySailConfiguration",
+                        "configuration": {
+                            "@type": f"{CONFIG_NS}.RDF4JPersistentInMemorySailConfiguration",
+                            "syncDelay": 1000,
+                            "directTypeInference": False,
+                            "inferencer": "none",
+                        },
+                    }
+                ),
+                "historyEnabled": "false",
+                "validationEnabled": "false",
+                "mdrRegistration": json.dumps(
+                    {
+                        "datasetAbstraction": f"{ST_URL}/mdr/dataset-{taxonomy_name}-abs",
+                        "version": "1.0.0",
+                        "currentVersion": True,
+                    }
+                ),
+                "openAtStartup": "true",
+                # facets
+                # {"@type":"it.uniroma2.art.semanticturkey.settings.facets.ProjectFacets"}
+                # createMainShard
+                # false
+                # appCtx
+                # SHOWVOC
+                # blacklistingEnabled
+                # false
+            },
+        )
+        is_response_ok("Create Project", res)
+
+        res = session.post(f"{CORE_SERVICES_BASE}/Projects/makePublic?ctx_project={taxonomy_name}")
+        if is_response_ok("Make Public", res):
             res = session.post(
-                f"{CORE_SERVICES_BASE}/Settings/storeSettingDefault?ctx_project={taxonomy_name}",
-                data={
-                    "componentID": f"{ST_NS}.settings.core.SemanticTurkeyCoreSettingsManager",
-                    "scope": "PROJECT_USER",
-                    "defaultScope": "PROJECT",
-                    "propertyName": "activeSchemes",
-                    "propertyValue": f'["<{str(OFF[taxonomy_name])}>"]',
-                },
+                f"{CORE_SERVICES_BASE}/InputOutput/clearData?ctx_project={taxonomy_name}"
             )
-            is_response_ok("Set active scheme", res)
+            if is_response_ok("Clear Data", res):
+                with open(f"{taxonomy_name}.ttl", "rb") as f:
+                    res = session.post(
+                        f"{CORE_SERVICES_BASE}/InputOutput/loadRDF?ctx_project={taxonomy_name}",
+                        # /st-core-services/InputOutput/loadRDF?ctx_project=countries&ctx_forceEditable=true&ctx_shard=main
+                        data={
+                            "baseURI": BASE_URI,
+                            "format": "Turtle",
+                            "consumer": "SYSTEM",
+                            "transitiveImportAllowance": "web",
+                            # validateImplicitly
+                            # false
+                        },
+                        files={"inputFile": f},
+                    )
+                    # Load service just returns a result object which is empty on success
+                    res_json = res.json()
+                    if res_json.get("result", res_json):
+                        print(f"Load RDF: result: {res_json}")
+
+                res = session.post(
+                    (
+                        f"{CORE_SERVICES_BASE}/Settings/storeSettingDefault"
+                        f"?ctx_project={taxonomy_name}"
+                    ),
+                    data={
+                        "componentID": f"{ST_NS}.settings.core.SemanticTurkeyCoreSettingsManager",
+                        "scope": "PROJECT_USER",
+                        "defaultScope": "PROJECT",
+                        "propertyName": "activeSchemes",
+                        "propertyValue": f'["<{str(OFF[taxonomy_name])}>"]',
+                    },
+                )
+                is_response_ok("Set active scheme", res)
